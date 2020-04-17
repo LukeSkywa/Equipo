b58e4e2160f200dcf87fac41ce65f19bb232bc30
AndreaLoNivuro <a.lonivuro@live.it>
2020-04-17 11:26:41 +0200

    creati componenti barra menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componets/login/login.component';
 import { HomepageComponent } from './componets/homepage/homepage.component';
+import { MenuComponent } from './componets/menu/menu.component';
+import { ListaComponent } from './componets/lista/lista.component';
+import { CardsComponent } from './componets/cards/cards.component';
+import { FeedbackComponent } from './componets/feedback/feedback.component';
+import { ProfiloComponent } from './componets/profilo/profilo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomepageComponent
+    HomepageComponent,
+    MenuComponent,
+    ListaComponent,
+    CardsComponent,
+    FeedbackComponent,
+    ProfiloComponent
   ],
   imports: [
     BrowserModule,
