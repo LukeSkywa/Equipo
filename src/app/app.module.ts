85ccd948f8ac6b82ea63657124a56e64acb7d8bb
Facco99 <facchin810@gmail.com>
2020-04-17 16:54:01 +0200

    cambiato nome cartella components e aggiunta angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,20 @@ import { LoginComponent } from './componets/login/login.component';
 import { HomepageComponent } from './componets/homepage/homepage.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { MatSliderModule } from '@angular/material/slider';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSelectModule} from '@angular/material/select';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 import { MenuComponent } from './componets/menu/menu.component';
 import { ListaComponent } from './componets/lista/lista.component';
 import { CardsComponent } from './componets/cards/cards.component';
@@ -27,7 +41,20 @@ import { ProfiloComponent } from './componets/profilo/profilo.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatMenuModule,
+    MatRadioModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
