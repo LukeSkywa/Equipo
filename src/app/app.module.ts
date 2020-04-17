309b06d5faf9c970746162f7b279eb832405e58f
Facco99 <facchin810@gmail.com>
2020-04-17 17:37:43 +0200

    sistemazione errori

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoginComponent } from './components/login/login.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -28,16 +27,7 @@ import { CardsComponent } from './components/cards/cards.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
 import { ProfiloComponent } from './components/profilo/profilo.component';
 import { RegistrazioneComponent } from './components/registrazione/registrazione.component';
-=======
-import { LoginComponent } from './componets/login/login.component';
-import { HomepageComponent } from './componets/homepage/homepage.component';
-import { MenuComponent } from './componets/menu/menu.component';
-import { ListaComponent } from './componets/lista/lista.component';
-import { CardsComponent } from './componets/cards/cards.component';
-import { FeedbackComponent } from './componets/feedback/feedback.component';
-import { ProfiloComponent } from './componets/profilo/profilo.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
->>>>>>> Homepage&Menu
+
 
 @NgModule({
   declarations: [
@@ -54,7 +44,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     ReactiveFormsModule,
     MatSliderModule,
     MatToolbarModule,
@@ -68,10 +57,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatRadioModule,
     MatSelectModule,
     BrowserAnimationsModule,
-
-=======
-    BrowserAnimationsModule,
->>>>>>> Homepage&Menu
   ],
   providers: [],
   bootstrap: [AppComponent]
