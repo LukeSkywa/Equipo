94a330cf5f8fab6837b8b7111f6f6d2095a9ce96
Facco99 <facchin810@gmail.com>
2020-04-20 17:21:21 +0200

    nascosto footer da login e registrazione

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  currentRoute:string=null;
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe(value => {
+      this.currentRoute=this.router.url.toString();
+    });
   }
 
 }
