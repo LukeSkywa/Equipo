cd35d5357e2cb2517962c7f88fba00c6109137a7
MorPeindaDiop <61980976+MorPeindaDiop@users.noreply.github.com>
2020-04-22 09:51:14 +0200

    viaggi ts

---
+++ src/app/models/viaggi.ts
@@ -0,0 +1,15 @@
+export interface Viaggi {
+ 
+    id: number;
+    immagine:string;
+    nome:String;
+    genere:String;
+    description: string;
+    voto:string;
+    prezzo:number;
+    annoUscita:string;
+    preferito:boolean;
+    nascosto:boolean;
+    sviluppatore:String;
+    editore:String;
+}
