11b2f7a89696b65153b5c6705e36b9c545bd8e52
Facco99 <facchin810@gmail.com>
2020-04-26 16:08:43 +0200

    Errori di ortografia sistemati

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { ListaComponent } from './components/lista/lista.component';
-import { CardsComponent } from './components/cards/cards.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
 import { ProfiloComponent } from './components/profilo/profilo.component';
 import { LoginGuard } from './guards/login.guard';
@@ -20,7 +19,6 @@ const routes: Routes = [
   { path: 'list', component: ListaComponent,canActivate:[LoginGuard] },
   { path: 'list/:localita', component: ListaComponent,canActivate:[LoginGuard] },
   { path: 'dettaglio/:id', component: DettaglioViaggioComponent,canActivate:[LoginGuard] },
-  { path: 'cards', component: CardsComponent,canActivate:[LoginGuard] },
   { path: 'feedback', component: FeedbackComponent,canActivate:[LoginGuard] },
   { path: 'profilo', component: ProfiloComponent,canActivate:[LoginGuard] },
   { path: 'modifica-profilo', component: ModificaProfiloComponent,canActivate:[LoginGuard] },
