8878cf4c6915ffa152f2d363695aba3bd2edd258
Facco99 <facchin810@gmail.com>
2020-06-03 10:01:32 +0200

    generate login

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -5,10 +5,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './homepage/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ListaComponent } from './lista/lista.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, ListaComponent],
+  declarations: [HomeComponent, ListaComponent, LoginComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
---
+++ src/app/features/home/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProfiloService } from 'src/app/services/profilo.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  hide: boolean = true;
+
+  get emailControl(): FormControl {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get passwordControl(): FormControl {
+    return this.loginForm.get('password') as FormControl;
+  }
+
+  constructor(private fb: FormBuilder, private profiloService: ProfiloService, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.profiloService.eseguiLogin(this.emailControl.value, this.passwordControl.value);
+  }
+
+}
